bb831f82679d96306640cefadfc066521a45c42e
Gian Spoletini <109772801+gianspo@users.noreply.github.com>
2023-01-12 18:20:32 -0300

    Add files via upload

---
+++ create_tables.sql
@@ -0,0 +1,3 @@
+CREATE TABLE jobs(id int,job varchar(255));
+CREATE TABLE hired_employees(id int,name varchar(255),datetime varchar(255),department_id int, job_id int);
+CREATE TABLE deparments(id int,department varchar(255));
